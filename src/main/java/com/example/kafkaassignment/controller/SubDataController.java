bfa332c4d8311116c930a99a0449202ae182c302
Kumar <abhinavkumar392@deloitte.com>
2023-12-20 12:20:01 +0530

    first commit

---
+++ src/main/java/com/example/kafkaassignment/controller/SubDataController.java
@@ -0,0 +1,7 @@
+package com.example.kafkaassignment.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SubDataController {
+}
---
+++ src/test/java/com/example/kafkaassignment/KafkaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
