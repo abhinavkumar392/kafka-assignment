75456f3ac9f342ecad47601c37bc8b3967b05438
Kumar <abhinavkumar392@deloitte.com>
2023-12-21 10:30:15 +0530

    configured changes

--- src/main/java/com/example/kafkaassignment/config/KafkaTopicConfig.java
+++ src/main/java/com/example/kafkaassignment/config/KafkaTopicConfig.java
@@ -1,5 +1,6 @@
 package com.example.kafkaassignment.config;
 
+import com.example.kafkaassignment.constants.AppConstants;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,23 @@ public class KafkaTopicConfig {
     @Bean
     public NewTopic kafkaAssignmentTopic() {
         return TopicBuilder.name("Auth-Topic").build();
+    }
+
+    @Bean
+    public NewTopic serviceTopic() {
+        return TopicBuilder.name(AppConstants.ServiceTopic).build();
+    }
+
+    @Bean
+    public NewTopic subscriberTopic() {
+        return TopicBuilder.name(AppConstants.SubscriberTopic).build();
+    }
+
+    @Bean
+    public NewTopic patientTopic() {return TopicBuilder.name(AppConstants.PatientTopic).build();}
+
+    @Bean
+    public NewTopic caseInfoTopic() {
+        return TopicBuilder.name(AppConstants.CaseTopic).build();
     }
 }
--- src/main/java/com/example/kafkaassignment/service/StreamService.java
+++ src/main/java/com/example/kafkaassignment/service/StreamService.java
@@ -6,7 +6,6 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 import org.slf4j.Logger;
@@ -16,6 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.Properties;
 
 @Service
+//@EnableKafkaStreams
 public class StreamService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StreamService.class);
@@ -24,31 +24,39 @@ public class StreamService {
 
         try {
             StreamsBuilder builder = new StreamsBuilder();
-            KStream<String,String> stream = builder.stream(AppConstants.AuthTopic, Consumed.with(Serdes.String(),Serdes.String()));
+            Properties properties = new Properties();
+            properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+            properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "json-processor");
+            properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+            properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+            KStream<String, String> stream = builder.stream("Auth-Topic");
 
             LOGGER.info("stream start");
 
             stream
-                    .filter((key, value) -> value.contains("\"SUB\":"))
+                    .filter((key, value) -> value.contains("\" SUB\":"))
                     .mapValues(value -> substrx(value, "\"SUB\":"))
-                    .to(AppConstants.SubscriberTopic);
+                    .to(AppConstants.SubscriberTopic,Produced.with(Serdes.String(),Serdes.String()));
 
             stream
                     .filter((key, value) -> value.contains("\"CAS\":"))
                     .mapValues(value -> substrx(value, "\"CAS\":"))
-                    .to(AppConstants.CaseTopic);
+                    .to(AppConstants.CaseTopic,Produced.with(Serdes.String(),Serdes.String()));
 
             stream
                     .filter((key, value) -> value.contains("\"SVC\":"))
                     .mapValues(value -> substrx(value, "\"SVC\":"))
-                    .to(AppConstants.ServiceTopic);
+                    .to(AppConstants.ServiceTopic,Produced.with(Serdes.String(),Serdes.String()));
 
             stream
                     .filter((key, value) -> value.contains("\"PAT\":"))
                     .mapValues(value -> substrx(value, "\"PAT\":"))
-                    .to(AppConstants.PatientTopic);
+                    .to(AppConstants.PatientTopic,Produced.with(Serdes.String(),Serdes.String()));
 
-            LOGGER.info("stream end");
+            Topology topo = builder.build();
+            KafkaStreams streams = new KafkaStreams(topo, properties);
+            streams.start();
 
         } catch (Exception e) {
             LOGGER.error("Runtime Exception",e);
